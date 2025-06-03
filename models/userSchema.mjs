3a16230ce41fdff1fa55b8ad41b273e37a775590
Geiselle Holt <geiselleholt@hotmail.com>
2025-06-03 15:01:56 -0600

    fixed user routes and user schema, tested and working!

--- models/userSchema.mjs
+++ models/userSchema.mjs
@@ -3,12 +3,22 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   userName: { type: String, required: true, unique: true },
   password: { type: String, required: true, minLength: 3 },
-  questions: [
-    {
-      question: { type: String, required: true },
-      answer: { type: String, required: true },
+  securityQuestions: {
+    type: [
+      {
+        question: { type: String, required: true },
+        answer: { type: String, required: true },
+      },
+    ],
+    _id: false,
+    required: true,
+    validate: {
+      validator: function (arr) {
+        return arr.length === 3;
+      },
+      message: `Please provide exactly 3 security questions.`,
     },
-  ],
+  },
 });
 
 export default mongoose.model("User", userSchema);
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -13,55 +13,69 @@ const router = express.Router();
 // @route: POST /api/user/register
 // @desc:  CREATE a register user route
 // @access: Public
-router.post("/register", async (req, res) => {
-  const { userName, password, questions } = req.body; // destructure request body
+router.post("/register", async (req, res, next) => {
+  const { userName, password, securityQuestions } = req.body; // destructure request body
 
-  if (!userName || !password || !questions) {
-    return res.status(400).json({
-      msg: "Fill in all required fields",
-    });
+  try {
+    if (!userName || !password || !securityQuestions) {
+      return res.status(400).json({
+        msg: "Fill in all required fields",
+      });
+    }
+
+    let user = await User.findOne({ userName });
+    if (user) {
+      return res.status(400).json({ msg: "User name already exists" });
+    }
+
+    user = new User({ userName, password, securityQuestions });
+
+    await user.save();
+
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
   }
-
-  let user = await User.findOne({ userName });
-  if (user) {
-    return res.status(400).json({ msg: "User name already exists" });
-  }
-
-  user = new User({ userName, password, questions });
-
-  res.status(201).json({ user });
 });
 
 // @route: POST /api/user/login
-// @desc:  CREATE a login user route
+// @desc:  Authenticate user and log them in
 // @access: Public
-router.post("/login", async (req, res) => {
+router.post("/login", async (req, res, next) => {
   const { userName, password } = req.body; // destructure request body
 
-  if (!userName || !password) {
-    return res.status(400).json({
-      msg: "All fields are required",
-    });
-  }
+  try {
+    if (!userName || !password) {
+      return res.status(400).json({
+        msg: "All fields are required",
+      });
+    }
 
-  let user = await User.findOne({ userName });
-  if (!user) {
-    return res.status(400).json({ msg: "Invalid Credentials" });
-  }
+    let user = await User.findOne({ userName });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
 
-  if (password != user.password) {
-    return res.status(400).json({ msg: "Invalid Credentials" });
-  }
+    if (password != user.password) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
 
-  res.status(200).json({ userId: user._id });
+    res.status(200).json({ userId: user._id });
+  } catch (error) {
+    next(error);
+  }
 });
 
-// @route: GET /api/user/login
+// @route: GET /api/user/all
 // @desc:  READ all user data
-// @access: Private
-router.get("/", async (req, res) => {
-  let allUsers = await User.find({});
-  res.status(200).json(allUsers);
+// @access: Public (for dev testing, not to be used on the FrontEnd)
+router.get("/all", async (req, res) => {
+  try {
+    let allUsers = await User.find({});
+    res.status(200).json(allUsers);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
